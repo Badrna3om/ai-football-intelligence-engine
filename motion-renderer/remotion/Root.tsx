ec1a2aaa18744a8a80f3ae053de69bbc7a5a050e
Badreldin Mohamed Awad <Badrna3om@gmail.com>
2026-09-07 02:12:01 +0400

    Add TACTIC Remotion root

---
+++ motion-renderer/remotion/Root.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Composition} from "remotion";
+import {TacticMatch,tacticMatchSchema,defaultMatch,calculateDuration} from "./TacticMatch";
+
+export const RemotionRoot:React.FC=()=> (
+  <Composition
+    id="TacticMatch"
+    component={TacticMatch}
+    durationInFrames={1590}
+    fps={30}
+    width={1080}
+    height={1920}
+    schema={tacticMatchSchema}
+    defaultProps={defaultMatch}
+    calculateMetadata={({props})=>({durationInFrames:calculateDuration(props)})}
+  />
+);
