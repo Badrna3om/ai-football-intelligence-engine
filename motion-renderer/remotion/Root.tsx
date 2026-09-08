1061639122048acac66bd3d9e4ba54bf34e2b43a
Badreldin Mohamed Awad <Badrna3om@gmail.com>
2026-09-09 02:23:04 +0400

    feat(motion): switch isolated V6 branch to full match composition

--- motion-renderer/remotion/Root.tsx
+++ motion-renderer/remotion/Root.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {Composition} from "remotion";
-import {TacticMatch,tacticMatchSchema,defaultMatch,calculateDuration} from "./TacticMatchV6Proof";
+import {TacticMatch,tacticMatchSchema,defaultMatch,calculateDuration} from "./TacticMatchV6Full";
 
 export const RemotionRoot:React.FC=()=> (
   <Composition
     id="TacticMatch"
     component={TacticMatch}
-    durationInFrames={1590}
+    durationInFrames={1770}
     fps={30}
     width={1080}
     height={1920}
