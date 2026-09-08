26839f8fe5851669fca35dcaca34e2e740a11cf3
Badreldin Mohamed Awad <Badrna3om@gmail.com>
2026-09-08 04:16:23 +0400

    Route TacticMatch composition to cinematic v2

--- motion-renderer/remotion/Root.tsx
+++ motion-renderer/remotion/Root.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Composition} from "remotion";
-import {TacticMatch,tacticMatchSchema,defaultMatch,calculateDuration} from "./TacticMatch";
+import {TacticMatch,tacticMatchSchema,defaultMatch,calculateDuration} from "./TacticMatchV2";
 
 export const RemotionRoot:React.FC=()=> (
   <Composition
