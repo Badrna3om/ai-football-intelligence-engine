ff5fc0ddc92b6cf159199ab524dfa49de9350c7c
Badreldin Mohamed Awad <Badrna3om@gmail.com>
2026-09-09 23:00:12 +0400

    fix(motion): use runtime asset wrapper for stats v3

--- motion-renderer/remotion/Root.tsx
+++ motion-renderer/remotion/Root.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {Composition} from "remotion";
 import {tacticMatchSchema,defaultMatch} from "./TacticMatchV6Full";
-import {TacticStatsMotionV3,STATS_V3_FRAMES} from "./TacticStatsMotionV3";
+import {TacticStatsMotionV3Runtime,STATS_V3_FRAMES} from "./TacticStatsMotionV3Runtime";
 
 export const RemotionRoot:React.FC=()=> (
   <Composition
     id="TacticMatch"
-    component={TacticStatsMotionV3}
+    component={TacticStatsMotionV3Runtime}
     durationInFrames={STATS_V3_FRAMES}
     fps={30}
     width={1080}
