9884b2a7dc6574c3be1939d108dea90ef8e61068
Badreldin Mohamed Awad <Badrna3om@gmail.com>
2026-09-09 23:05:04 +0400

    fix(stats): localize TACTIC logo and MOTM cutout for Remotion

--- motion-renderer/remotion/TacticStatsMotionV3Runtime.tsx
+++ motion-renderer/remotion/TacticStatsMotionV3Runtime.tsx
@@ -1,20 +1,21 @@
 import React from "react";
+import {staticFile} from "remotion";
 import type {MatchProps} from "./TacticMatchV6Full";
 import {TacticStatsMotionV3,STATS_V3_FRAMES} from "./TacticStatsMotionV3";
 
-const TACTIC_LOGO_URL = "https://drive.usercontent.google.com/download?id=1njAsb_x6rN-el66QpAFKZ0aXWZZYU6Xl&export=download&confirm=t";
-const JACKSON_CUTOUT_URL = "https://drive.usercontent.google.com/download?id=19XwXNeGV3JdX2G-_EjLglo8c-QVIvYon&export=download&confirm=t";
-
 export const TacticStatsMotionV3Runtime: React.FC<MatchProps> = (props) => {
+  const tacticLogoUrl = staticFile("TACTIC_SPORT_logo.png");
+  const jacksonCutoutUrl = staticFile("jackson_cutout.png");
+
   const patched: MatchProps = {
     ...props,
     assets: {
       ...(props.assets || {}),
-      tacticLogoUrl: TACTIC_LOGO_URL,
-      starPlayerPhotoUrl: JACKSON_CUTOUT_URL,
+      tacticLogoUrl,
+      starPlayerPhotoUrl: jacksonCutoutUrl,
     },
     starPlayer: props.starPlayer
-      ? {...props.starPlayer, photoUrl: JACKSON_CUTOUT_URL}
+      ? {...props.starPlayer, photoUrl: jacksonCutoutUrl}
       : props.starPlayer,
   };
 
