89d5942f4aac3d94b4f75e6413451a8a936a23e8
Badreldin Mohamed Awad <Badrna3om@gmail.com>
2026-09-10 05:40:49 +0400

    feat(motion): route default renderer by compositionId

--- motion-renderer/remotion/Root.tsx
+++ motion-renderer/remotion/Root.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import {Composition} from "remotion";
-import {TacticMatch,tacticMatchSchema,defaultMatch,calculateDuration} from "./TacticMatchV3";
+import {
+  TacticMotionRouter,
+  tacticMotionRouterSchema,
+  defaultRouterProps,
+  calculateRouterDuration,
+} from "./TacticMotionRouter";
 import {
   TacticStatsHilalReference,
   tacticStatsHilalReferenceSchema,
@@ -12,14 +17,14 @@ export const RemotionRoot:React.FC=()=> (
   <>
     <Composition
       id="TacticMatch"
-      component={TacticMatch}
+      component={TacticMotionRouter}
       durationInFrames={1590}
       fps={30}
       width={1080}
       height={1920}
-      schema={tacticMatchSchema}
-      defaultProps={defaultMatch}
-      calculateMetadata={({props})=>({durationInFrames:calculateDuration(props)})}
+      schema={tacticMotionRouterSchema}
+      defaultProps={defaultRouterProps}
+      calculateMetadata={({props})=>({durationInFrames:calculateRouterDuration(props)})}
     />
     <Composition
       id="TacticStatsHilalReference"
