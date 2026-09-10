0fb9df07ddab6c03e09438e01146a1311a5d3787
Badreldin Mohamed Awad <Badrna3om@gmail.com>
2026-09-10 06:01:44 +0400

    feat(motion): register stacked Hilal stats V2

--- motion-renderer/remotion/Root.tsx
+++ motion-renderer/remotion/Root.tsx
@@ -12,6 +12,12 @@ import {
   defaultHilalStatsProps,
   calculateHilalStatsDuration,
 } from "./stats-hilal-reference/TacticStatsHilalReference";
+import {
+  TacticStatsHilalStackV2,
+  tacticStatsHilalStackV2Schema,
+  defaultHilalStackV2Props,
+  calculateHilalStackV2Duration,
+} from "./stats-hilal-reference/TacticStatsHilalStackV2";
 
 export const RemotionRoot:React.FC=()=> (
   <>
@@ -36,6 +42,17 @@ export const RemotionRoot:React.FC=()=> (
       schema={tacticStatsHilalReferenceSchema}
       defaultProps={defaultHilalStatsProps}
       calculateMetadata={({props})=>({durationInFrames:calculateHilalStatsDuration(props)})}
+    />
+    <Composition
+      id="TacticStatsHilalStackV2"
+      component={TacticStatsHilalStackV2}
+      durationInFrames={645}
+      fps={30}
+      width={1080}
+      height={1920}
+      schema={tacticStatsHilalStackV2Schema}
+      defaultProps={defaultHilalStackV2Props}
+      calculateMetadata={()=>({durationInFrames:calculateHilalStackV2Duration()})}
     />
   </>
 );
